710d1b641b3bd07ea366e2ab87b37824a1203a5c
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-06 17:18:11 +0900

    [feat] buildSrc 모듈 초기화

---
+++ buildSrc/src/main/java/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    const val packageName = "com.example.todaysweather"
+    const val compileSdk = 32
+    const val minSdk = 24
+    const val targetSdk = 32
+    var versionCode = 1
+    var versionName = "1.0.0"
+}
