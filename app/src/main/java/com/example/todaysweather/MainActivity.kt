f53b227e8d646307267191606e22acdc2692892f
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-10 18:55:36 +0900

    [feat] MainActivity 추가

---
+++ app/src/main/java/com/example/todaysweather/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.todaysweather
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.todaysweather.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
