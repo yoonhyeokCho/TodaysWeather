d8c77657a41b0284df581ffb263224b1d0804dbb
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-10 19:41:36 +0900

    [feat] core-ui 모듈 초기 세팅

---
+++ core-ui/src/main/java/com/example/core_ui/base/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.yangbong.core_ui.base
+
+import androidx.lifecycle.ViewModel
+abstract class BaseViewModel : ViewModel() {
+
+}
---
+++ core-ui/src/main/java/com/example/core_ui/util/Injector.kt
@@ -0,0 +1,16 @@
+package com.example.core_ui.util
+
+
+import com.example.navigator.MainNavigator
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+sealed interface Injector {
+    @EntryPoint
+    @InstallIn(ActivityComponent::class)
+    interface MainNavigatorInjector {
+        fun mainNavigator(): MainNavigator
+    }
+
+}
