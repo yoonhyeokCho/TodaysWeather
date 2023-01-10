093fa1b1e2661209665071e42a8cf95614170737
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-10 19:48:53 +0900

    [feat] app 모듈에 MainNavigator 인터페이스 구현 클래스 추가

---
+++ app/src/main/java/com/example/todaysweather/navigator/MainNavigatorImpl.kt
@@ -0,0 +1,13 @@
+package com.example.todaysweather.navigator
+
+import android.content.Context
+import com.example.navigator.MainNavigator
+import com.example.todaysweather.MainActivity
+import com.yangbong.core_ui.extension.navigateActivity
+import javax.inject.Inject
+
+internal class MainNavigatorImpl @Inject constructor() : MainNavigator{
+    override fun navigateMain(context: Context) {
+        context.navigateActivity<MainActivity>()
+    }
+}
