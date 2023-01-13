e5013d74080256a0685bf288c944a72efe25deea
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-14 02:16:01 +0900

    [add] navigatior 모듈의 Navigatior.kt 에 MainActivity 이동시 home 화면이 나오도록 transaction 로직 추가

--- navigator/src/main/java/com/example/navigator/Navigator.kt
+++ navigator/src/main/java/com/example/navigator/Navigator.kt
@@ -5,7 +5,7 @@ import android.content.Context
 interface MainNavigator{
     // MainActivity로 이동
     fun navigateMain(context: Context)
-    
+
     /** MainActivity로 이동시 home화면이 나오도록 transaction 하는 로직 */
     fun transactionToHome()
 }
