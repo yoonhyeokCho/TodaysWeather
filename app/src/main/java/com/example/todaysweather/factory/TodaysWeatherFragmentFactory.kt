38bed2bd3a9d68acb8531c708d97f841412a90ca
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-16 20:39:37 +0900

    [fix] MainActivity 실행 시 Feature 모듈의 Fragment를 띄우기 위해 app 모듈의 factory 패키지의 TodaysWeatherFragmentFactory 수정

--- app/src/main/java/com/example/todaysweather/factory/TodaysWeatherFragmentFactory.kt
+++ app/src/main/java/com/example/todaysweather/factory/TodaysWeatherFragmentFactory.kt
@@ -3,9 +3,26 @@ package com.example.todaysweather.factory
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.example.core_ui.util.Injector
+import com.example.core_ui.util.ResolutionMetrics
+import com.example.home.HomeFragment
+import dagger.hilt.android.EntryPointAccessors
 
 class TodaysWeatherFragmentFactory(activity: AppCompatActivity) : FragmentFactory() {
+
+    private val resolutionMetrics: ResolutionMetrics by lazy {
+        EntryPointAccessors.fromActivity(
+            activity,
+            Injector.ResolutionMetricsInjector::class.java
+        ).resolutionMetrics()
+    }
+
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
-        return super.instantiate(classLoader, className)
+        return when(className){
+            HomeFragment::class.java.name -> HomeFragment(resolutionMetrics)
+            else -> super.instantiate(classLoader, className)
+        }
+
+
     }
 }
