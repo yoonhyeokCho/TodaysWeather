3a811ae5a2fdeaf010684c873fae1cf977fd1c01
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-16 20:36:22 +0900

    [add] app 모듈의 di 패키지에 displayMetrics를 제공하는 hilt AppModule 추가

---
+++ app/src/main/java/com/example/todaysweather/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.example.todaysweather.di
+
+import android.app.Application
+import android.util.DisplayMetrics
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideDisplayMetrics(app: Application): DisplayMetrics = app.resources.displayMetrics
+}
