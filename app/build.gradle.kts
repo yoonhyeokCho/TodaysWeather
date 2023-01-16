ea1a3d61dcdc72a4bb8fbc55a5725f8c8aabe14a
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-16 20:45:14 +0900

    [chore] app 모듈의 build.gradle.kts 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,11 +59,6 @@ dependencies {
 
     // Dagger-Hilt
     implementation(AndroidXDependencies.hilt)
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     kapt(KaptDependencies.hiltCompiler)
 
     // Jetpack Navigation Component
