c9364d0fb451f9879888dd8f8f220e33104cc43b
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-16 20:48:52 +0900

    [feat] feature 모듈의 HomeFragment.kt 셋팅

--- feature/src/main/java/com/example/home/HomeFragment.kt
+++ feature/src/main/java/com/example/home/HomeFragment.kt
@@ -3,12 +3,17 @@ package com.example.home
 import android.os.Bundle
 import android.view.View
 import com.example.core_ui.base.BindingFragment
+import com.example.core_ui.util.ResolutionMetrics
 import com.example.todaysweather.feature.R
 import com.example.todaysweather.feature.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment: BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment(private val resolutionMetrics: ResolutionMetrics):
+    BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    private val Number.dp get() = resolutionMetrics.toPixel(this.toInt())
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
